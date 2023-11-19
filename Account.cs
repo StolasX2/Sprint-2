f893fa21fbb1ea83fbe182313da26f1ee6d95c0d
tnich <tnich@DESKTOP-96I87U4>
2023-11-19 13:30:04 -0500

    Add project files.

---
+++ Account.cs
@@ -0,0 +1,56 @@
+﻿ public abstract class Account
+{
+    protected static long accountID = 0;
+    protected long phonenumber;
+    protected string email;
+
+    public Account(long phonenum, string em)
+    {
+        accountID = accountID++;
+        phonenumber = phonenum;
+        email = em;
+    }
+
+    public long getAccount()
+    {
+        return accountID;
+    }
+    public long getPhone()
+    {
+        return phonenumber;
+    }
+
+    public string getEmail()
+    {
+        return email;
+    }
+    public long setPhone(long p)
+    {
+        phonenumber = p;
+        return phonenumber;
+    }
+    public string setEmail(string e)
+    {
+        email = e;
+        return email;
+    }
+}
+
+public class Customer : Account
+{
+    public Customer(long phonenum, string em) : base( phonenum, em) 
+    {
+        accountID = accountID++;
+        phonenumber = phonenum;
+        email = em;
+    }
+}
+public class Employee : Account
+{
+    public Employee( long phonenum, string em) : base( phonenum, em)
+    {
+        accountID = accountID++;
+        phonenumber = phonenum;
+        email = em;
+    }
+}
