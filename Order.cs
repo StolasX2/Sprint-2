187a71c78e849f58a4da7b5aaa2dad4387d2c6d9
StolasX2 <trent.nicholson@yahoo.com>
2023-11-25 12:26:21 -0500

    new stuff

---
+++ Order.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Collections;
+
+ abstract class Order {
+    
+    protected static int ordernum;
+    protected LinkedList<Item> orderlist;
+    protected float checkouttotal;
+
+    public Order()
+    {
+        orderlist = new LinkedList<Item>();
+        checkouttotal = 0;
+        ordernum = ordernum++;
+    }
+
+    public float getTotal()
+    {
+        return checkouttotal;
+    }
+    public int getOrdernum()
+    {
+        return ordernum;
+    }
+    public LinkedList<Item> getItemList()
+    {
+        return orderlist;
+    }
+    public void addItem(Item newitem)
+    {
+        if(orderlist.Count < 4)
+        {
+            orderlist.AddLast(newitem);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public void removeItem(Item olditem) {
+        orderlist.Remove(olditem);
+    }
+}
---
+++ Payment.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class Payment
+{
+    private long confirmationNumber;
+    private float amount;
+
+    public Payment(long confirmationNumber, float amount)
+    {
+        this.confirmationNumber = confirmationNumber;
+        this.amount = amount;
+    }
+
+    public void Discount(int discountPercentage)
+    {
+        amount -= amount * (discountPercentage / 100.0f);
+    }
+
+    public bool IsPaid()
+    {
+        return amount <= 0;
+    }
+
+    public void Pay()
+    {
+        Console.WriteLine($"Payment of ${amount} processed successfully.");
+    }
+
+    public string GenerateReceipt()
+    {
+        string receipt = $"Receipt - Confirmation Number: {confirmationNumber}\n";
+        receipt += $"Amount Paid: ${amount}\n";
+        return receipt;
+    }
+
+    public bool IsDiscounted(string couponCode)
+    {
+        string[] validCouponCodes = { "DISCOUNT10", "SAVE20", "SPECIALOFFER" };
+        return Array.Exists(validCouponCodes, code => code == couponCode);
+    }
+
+    public static void Main()
+    {
+        Payment payment1 = new Payment(123456, 100.0f);
+        payment1.Discount(10);
+        Console.WriteLine(payment1.IsPaid());
+        payment1.Pay();
+        Console.WriteLine(payment1.IsPaid());
+        string receipt = payment1.GenerateReceipt();
+        Console.WriteLine(receipt);
+
+        string couponCode = "SAVE20";
+        Console.WriteLine(payment1.IsDiscounted(couponCode));
+    }
+}
